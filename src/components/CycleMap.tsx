0dcc1424b04a607aac2f7be9da2d906573904e6d
daisafernandes <daisafernandes@gmail.com>
2026-04-08 14:00:16 -0300

    Update CycleMap component to use OpenStreetMap and CARTO for tile layer attribution and URL

--- src/components/CycleMap.tsx
+++ src/components/CycleMap.tsx
@@ -82,8 +82,8 @@ const CycleMap = ({ ciclovias, selectedId, onSelect, flyTo }: CycleMapProps) => 
       attributionControl={true}
     >
       <TileLayer
-        attribution='&copy; <a href="https://stadiamaps.com/">Stadia</a>'
-        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
       />
       <FlyToLocation center={flyTo} />
       <FitBoundsToCiclovias ciclovias={ciclovias} />
