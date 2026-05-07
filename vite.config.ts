79f02ce043c1489519eaded2f8edd95e6aca26b4
daisafernandes <daisafernandes@gmail.com>
2026-05-07 20:15:31 -0300

    remove lib

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { VitePWA } from "vite-plugin-pwa";
-import { componentTagger } from "lovable-tagger";
 
 const pwaThemeColor = "#1e293b";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   server: {
     host: "::",
     port: 8080,
@@ -86,8 +85,7 @@ export default defineConfig(({ mode }) => ({
         navigateFallbackDenylist: [/^\/api\//],
       },
     }),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
